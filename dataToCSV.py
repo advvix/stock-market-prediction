1e3fec736be4002ce48bc8a08308438c13e4915b
Adrian Igras <48068613+advvix@users.noreply.github.com>
2020-12-06 12:53:20 +0100

    Update dataToCSV.py

--- dataToCSV.py
+++ dataToCSV.py
@@ -1,34 +1,14 @@
 import numpy as np
 import pandas as pd
-
 import os
-entries = os.listdir('lasthope/')
-# Import data
-print(entries)
+
+entries = os.listdir('allCompanySP500/')
 df = pd.DataFrame(columns = entries)
 
 for i in range(len(entries)):
-    data = pd.read_csv(f'lasthope/{entries[i]}')
+    data = pd.read_csv(f'allCompanySP500/{entries[i]}')
     n = data.shape[0]
     data = data['close'].values
     df[entries[i]] = data
 
-
-df.to_csv('last1.csv', index=False)
-
-# import numpy as np
-# import pandas as pd
-#
-# import os
-# entries = os.listdir('nowy/')
-# # Import data
-# print(entries)
-# df = pd.DataFrame(columns = entries)
-#
-# for i in range(len(entries)):
-#     print(entries[i])
-#     data = pd.read_csv(f'nowy/{entries[i]}')
-#     data = data['<CLOSE>'].values
-#     df[entries[i]] = data
-#
-# df.to_csv('out3.csv', index=False)
+df.to_csv('data.csv', index=False)
